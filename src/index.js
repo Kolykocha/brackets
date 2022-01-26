00578eee5c91c5c20f4395af7bba064e34fd9743
Kirill Balaganskii <kirillbalaghanskiii@gmail.com>
2022-01-26 19:19:46 +0600

    complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+    const config = new Map(bracketsConfig);
+
+    let arr = [];
+
+    for (let i = 0; i < str.length; i++) {
+        if (str[i] === config.get(arr[arr.length-1])) {
+            arr.pop();
+        } else {
+            arr.push(str[i]);
+        }
+    }
+    return arr.length === 0;
 }
